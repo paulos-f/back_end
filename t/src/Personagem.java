e847b2c2ecb1025aed059fa628e704e4e57389e2
paulos-f <133170300+paulos-f@users.noreply.github.com>
2024-08-29 09:38:15 -0300

    Delete t directory

--- t/src/Personagem.java
+++
@@ -1,9 +0,0 @@
-public class Personagem {
-    public String nome;
-    public int vida;
-
-    public void atacar(Personagem alvo){
-        System.out.println(alvo.nome + " foi atacado por " + this.nome);
-        alvo.vida -= 5;
-    }
-}
